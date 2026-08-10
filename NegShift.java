3b31166ffa6ddc70077d14145425ac793db195a3
Deeksha G <deekshagnaik2005@gmail.com>
2026-08-11 00:41:50 +0530

    NegShift

---
+++ NegShift.java
@@ -0,0 +1,11 @@
+public class NegShift {
+    public static void main(String[] args) {
+        int target = -5; // Binary: 11111111111111111111111111111011
+        
+        // Shift left by 1
+        int result = target << 1; 
+        
+        System.out.println("Result: " + result); // Outputs -10
+        System.out.println("Binary: " + Integer.toBinaryString(result));
+    }
+}
