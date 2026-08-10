4578195dd92877dfc12894593744e1b66d7bc4fe
Deeksha G <deekshagnaik2005@gmail.com>
2026-08-11 00:08:11 +0530

    pre and post fix problem

---
+++ ex3.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class ex3{
+    public static void main(String[] args) {
+       int a=10,b=10,c,d;//c=10,a=11
+       c=a++;//post value op
+       d=++b;//pre  op  value b=11 d=11
+       System.out.println(a+" "+b+" "+c+" "+d+" ");
+        
+    }
+}
+
+/*OP
+
+11 11 10 11*/
