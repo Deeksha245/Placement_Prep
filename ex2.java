c68163fc9ec4ca748f186fb3937eebab68f27e40
Deeksha G <deekshagnaik2005@gmail.com>
2026-08-11 00:01:22 +0530

    Add ex2 program

---
+++ ex2.java
@@ -0,0 +1,15 @@
+/*Codeforces 617A/* */
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class ex2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println((x+4)/5);
+        
+    }
+}
+
+
+/*5
+1*/
